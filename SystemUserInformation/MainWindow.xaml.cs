b7dd9b23933faaac099bcb163a2e220e0dcff282
Blubbaxx <19249053+Blubbaxx@users.noreply.github.com>
2018-03-28 13:28:43 +0200

    Add files via upload

---
+++ SystemUserInformation/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Principal;
+using Microsoft.Win32;
+using System.Management;
+
+
+namespace SystemUserInformation
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+            string user = principal.Identity.Name;
+
+            txtbxUser.Text = user;
+
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion");
+            string osInformation = (string)registryKey.GetValue("productName");
+
+            txtbxOS.Text = osInformation;
+
+
+            txtbxCPU.Text = GetProcessorInformation();
+
+
+            txtbxRAM.Text = GetPhysicalMemory();
+
+
+            txtbxVideo.Text = GetVideoAdapter();
+        }
+
+
+ 
+        public static String GetProcessorId()
+        {
+
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            String Id = String.Empty;
+            foreach (ManagementObject mo in moc)
+            {
+
+                Id = mo.Properties["processorID"].Value.ToString();
+                break;
+            }
+            return Id;
+
+        }
+
+
+        public static String GetProcessorInformation()
+        {
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            String info = String.Empty;
+            foreach (ManagementObject mo in moc)
+            {
+                string name = (string)mo["Name"];
+                name = name.Replace("(TM)", "™").Replace("(tm)", "™").Replace("(R)", "®").Replace("(r)", "®").Replace("(C)", "©").Replace("(c)", "©").Replace("    ", " ").Replace("  ", " ");
+
+                info = name + ", " + (string)mo["Caption"] + ", " + (string)mo["SocketDesignation"];
+                //mo.Properties["Name"].Value.ToString();
+                //break;
+            }
+            return info;
+        }
+
+
+        public static string GetPhysicalMemory()
+        {
+            ManagementScope oMs = new ManagementScope();
+            ObjectQuery oQuery = new ObjectQuery("SELECT Capacity FROM Win32_PhysicalMemory");
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher(oMs, oQuery);
+            ManagementObjectCollection oCollection = oSearcher.Get();
+
+            long MemSize = 0;
+            long mCap = 0;
+
+            // In case more than one Memory sticks are installed
+            foreach (ManagementObject obj in oCollection)
+            {
+                mCap = Convert.ToInt64(obj["Capacity"]);
+                MemSize += mCap;
+            }
+            MemSize = (MemSize / 1024) / 1024;
+            return MemSize.ToString() + "MB RAM";
+        }
+
+
+        public static string GetVideoAdapter()
+        {
+            
+            ManagementObjectSearcher objvide = new ManagementObjectSearcher("select * from Win32_VideoController");
+
+            string adapter = "";
+
+            foreach (ManagementObject obj in objvide.Get())
+            {
+                adapter = adapter + obj["Name"] + " " + (Convert.ToInt64(obj["AdapterRAM"])/1024)/1024 + "MB " + Environment.NewLine;
+            }
+
+
+            return adapter;
+        }
+    }
+}
